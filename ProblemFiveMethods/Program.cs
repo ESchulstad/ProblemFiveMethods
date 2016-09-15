1e0108be11c388854e47bd98c8f0bd855434e5c3
ESchulstad <eschulstad@gmail.com>
2016-09-14 22:36:56 -0400

    Initial Commit

---
+++ ProblemFiveMethods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemFiveMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNum = int.Parse(Console.ReadLine());
+            Console.WriteLine(Double(inputNum));
+        }
+
+        static int Double(int num)
+        {
+            int answer = num * 2;
+            return answer;
+        }
+    }
+}
